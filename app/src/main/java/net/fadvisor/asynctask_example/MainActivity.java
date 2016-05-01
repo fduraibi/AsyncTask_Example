f957018bcd7d9f1cfc8966b8d6f34018279fc267
Fahad Alduraibi <fadnix@gmail.com>
2016-04-30 20:44:31 -0400

    Added a prograss bar to make it more visual

--- app/src/main/java/net/fadvisor/asynctask_example/MainActivity.java
+++ app/src/main/java/net/fadvisor/asynctask_example/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,51 +14,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resetValues();
     }
 
     public void startSync(View view) {
         TextView textView1 = (TextView) findViewById(R.id.textView1);
         TextView textView2 = (TextView) findViewById(R.id.textView2);
+        ProgressBar progressBar1 = (ProgressBar) findViewById(R.id.progressBar1);
 
         textView1.setText("بدأ");
-        for (int i=0 ; i<10 ; i++) {
+        for (int i=1 ; i<51 ; i++) {
             textView2.setText(String.valueOf(i));
-            SystemClock.sleep(200);
+            progressBar1.setProgress(i);
+            SystemClock.sleep(100);
         }
-        textView1.setText("انتهى");
+        textView1.setText("خطوه 2");
     }
 
     public void startAsync(View view) {
         final TextView textView3 = (TextView) findViewById(R.id.textView3);
         final TextView textView4 = (TextView) findViewById(R.id.textView4);
+        final ProgressBar progressBar2 = (ProgressBar) findViewById(R.id.progressBar2);
 
         textView3.setText("بدأ");
         new AsyncTask<Void, Integer, Void>() {
             @Override
             protected Void doInBackground(Void...nothing) {
-                for (int i=0 ; i<10 ; i++) {
+                for (int i=1 ; i<51 ; i++) {
                     publishProgress(i);
-                    SystemClock.sleep(200);
+                    SystemClock.sleep(100);
                 }
                 return null;
             }
             protected void onProgressUpdate(Integer... value) {
                 textView4.setText(value[0].toString());
+                progressBar2.setProgress(value[0]);
             };
         }.execute();
 
-        textView3.setText("انتهى");
+        textView3.setText("خطوه 2");
     }
 
     public void startClear(View view) {
+        resetValues();
+    }
+
+    private void resetValues() {
         TextView textView1 = (TextView) findViewById(R.id.textView1);
         TextView textView2 = (TextView) findViewById(R.id.textView2);
         TextView textView3 = (TextView) findViewById(R.id.textView3);
         TextView textView4 = (TextView) findViewById(R.id.textView4);
+        ProgressBar progressBar1 = (ProgressBar) findViewById(R.id.progressBar1);
+        ProgressBar progressBar2 = (ProgressBar) findViewById(R.id.progressBar2);
 
-        textView1.setText("");
+        textView1.setText("العمل في الواجهة");
         textView2.setText("0");
-        textView3.setText("");
+        textView3.setText("العمل في الخلفية");
         textView4.setText("0");
+        progressBar1.setProgress(0);
+        progressBar2.setProgress(0);
     }
 }
